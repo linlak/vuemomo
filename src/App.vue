8923720d91544890244b31943c5057651efebd5d
Linus Nowomukama <ugsalons@gmail.com>
2020-02-15 21:50:41 -0800

    save

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MomoQrPayment externalId="201-2445-123" amount="10"></MomoQrPayment>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MomoQrPayment from './components/MomoQrPayment.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MomoQrPayment
   }
 }
 </script>
---
+++ src/components/MomoQrPayment.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mobile-money-qr-payment"
+  data-amount="amount"
+  data-api-user-id="'apiUserId'"
+  data-external-id="externalId">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'momoqrpayment',
+  props: {
+    externalId: String,
+    amount: Number
+  }
+}
+</script>
+
+<style scoped>
+    .mobile-money-qr-payment {
+        width: 200px;
+        display: inline-block;
+    }
+</style>
